962bd1c9442d7c2b100c98cfa45be6e9e59ed2ad
hanzadahannie <hanzadaakylbekovna@mail.com>
2023-07-11 12:43:39 +0600

    hw1

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+// let a=4
+//  let b=55
+//  console.log(a/b)
+
+ // let a=4
+ // let b=55
+ // console.log(a*b)
+ // let a=4
+ // let b=55
+ // console.log(a+b)
+ // let a=4
+ // let b=55
+ // console.log(a%b)
+
+// let a="hello"
+//  let b="hannie"
+//  console.log(a+" "+b)
+// let a=8
+//  let b="9abs"
+//  console.log(a*b)
+// let a=5
+//  let b='5'
+//  console.log(a+ +b)
+//  let a=2
+//  let b
+//      console.log(a*b)
